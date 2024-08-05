b1185821c8796564b5990fb95407fc77552c4b74
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-04 22:43:28 -0300

    feat: initialized jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
-import { register } from './controllets/register-controller'
-import { authenticate } from './controllets/authenticate-controller'
+import { register } from './controllets/register'
+import { authenticate } from './controllets/authenticate'
+import { profile } from './controllets/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // authenticated
+  app.get('/me', profile)
 }
