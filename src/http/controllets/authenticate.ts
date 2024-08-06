0a9ccc1fdd69c688fecd3ee98b327fe38bc97890
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-06 00:40:24 -0300

    feat: added controller for authenticate with jwt

--- src/http/controllets/authenticate.ts
+++ src/http/controllets/authenticate.ts
@@ -17,7 +17,15 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({ email, password })
+    const {user} = await authenticateUseCase.execute({ email, password })
+
+    // jamais colocar info sensiveis
+    const token =  await reply.jwtSign({}, {
+      sign: {
+        sub: user.id
+      }
+    })
+    return reply.status(200).send({token})
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
@@ -26,5 +34,4 @@ export async function authenticate(
     throw error
   }
 
-  return reply.status(200).send()
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllets/register'
 import { authenticate } from './controllets/authenticate'
 import { profile } from './controllets/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   // authenticated
-  app.get('/me', profile)
+  app.get('/me', {onRequest: [verifyJWT]} ,profile)
 }
