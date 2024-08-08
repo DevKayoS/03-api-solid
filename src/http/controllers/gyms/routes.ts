69311f32912889c6f331837c29e0373ef2488ca6
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-08 00:13:07 -0300

    fix: ajusting method

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -9,5 +9,6 @@ export async function gymsRoutes(app: FastifyInstance) {
   
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
-  app.get('/gyms/create', create)
+  
+  app.post('/gyms/create', create)
 }
